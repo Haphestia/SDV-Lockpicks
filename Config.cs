ff30a9dda78fdc58479720de72df3c3dbd574c97
bwdymods <bwdymods@gmail.com>
2019-02-22 22:17:12 -0900

    version 0.2.0

---
+++ Config.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Lockpicks
+{
+    public class ConfigLockEnd
+    {
+        public string MapName;
+        public int MapX;
+        public int MapY;
+        [JsonIgnore]
+        public bool OutLock = true;
+        public ConfigLockEnd() { }
+        public ConfigLockEnd(string mn, int x, int y)
+        {
+            MapName = mn;
+            MapX = x;
+            MapY = y;
+        }
+
+        public ConfigLockEnd(string str)
+        {
+            string[] ss = str.Split('.');
+            MapName = ss[0];
+            MapX = int.Parse(ss[1]);
+            MapY = int.Parse(ss[2]);
+        }
+
+        public string str()
+        {
+            return MapName + "." + MapX + "." + MapY;
+        }
+    }
+
+    class Config
+    {
+        public static Dictionary<string, string> OutLocks;
+        public static Dictionary<string, string> InLocks;
+        public static bool ready = false;
+        public static void Load()
+        {
+            try
+            {
+                string filecontents = File.ReadAllText(Mod.Instance.Helper.DirectoryPath + Path.DirectorySeparatorChar + "out_locks.json");
+                OutLocks = JsonConvert.DeserializeObject<Dictionary<string, string>>(filecontents);
+                filecontents = File.ReadAllText(Mod.Instance.Helper.DirectoryPath + Path.DirectorySeparatorChar + "in_locks.json");
+                InLocks = JsonConvert.DeserializeObject<Dictionary<string, string>>(filecontents);
+                //add inverse inlocks
+                foreach (var l in InLocks.Keys.ToArray())
+                {
+                    InLocks[InLocks[l]] = l;
+                }
+                ready = true;
+            }
+            catch (Exception e)
+            {
+                Mod.Instance.Monitor.Log("Failed to read lock config file: " + e.Message, StardewModdingAPI.LogLevel.Error);
+            }
+        }
+
+        public static ConfigLockEnd GetMatchingOutLock(string key)
+        {
+            if (OutLocks.ContainsKey(key)) return new ConfigLockEnd(OutLocks[key]);
+            return null;
+        }
+
+        public static ConfigLockEnd GetMatchingInLock(string key)
+        {
+            if (InLocks.ContainsKey(key))
+            {
+                var c2 = new ConfigLockEnd(InLocks[key]);
+                c2.OutLock = false;
+                return c2;
+            }
+            return null;
+        }
+    }
+}
---
+++ Mod.cs
@@ -0,0 +1,108 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using StardewModdingAPI.Events;
+using System.Collections.Generic;
+
+namespace Lockpicks
+{
+    public class Mod : StardewModdingAPI.Mod
+    {
+#if DEBUG
+        private static readonly bool DEBUG = true;
+#else
+        private static readonly bool DEBUG = false;
+#endif
+        public static Mod Instance;
+        public static bwdyworks.ModUtil ModUtil;
+
+        public override void Entry(IModHelper helper)
+        {
+            ModUtil = new bwdyworks.ModUtil(this);
+            Instance = this;
+            if(ModUtil.StartConfig(DEBUG))
+            {
+                Config.Load();
+                if (Config.ready)
+                {
+                    ModUtil.AddItem(new bwdyworks.BasicItemEntry(this, "lockpick", 30, -300, "Basic", Object.junkCategory, "Lockpick", "Used to bypass locked doors."));
+                    ModUtil.AddMonsterLoot(new bwdyworks.MonsterLootEntry(this, "Green Slime", "lockpick", 0.1f));                 
+                }
+                helper.Events.Input.ButtonPressed += Input_ButtonPressed;
+                ModUtil.EndConfig();
+            }
+        }
+
+        private void Input_ButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (ModUtil.Debug)
+            {
+                if (e.Button == SButton.NumPad0)
+                {
+                    var id = ModUtil.GetModItemId("lockpick");
+                    if (ModUtil.Debug && id != null) Monitor.Log("lockpick id: " + id.Value);
+
+                    if (id != null) ModUtil.GiveItemToLocalPlayer(id.Value, 1);
+                }
+            }
+            if (e.Button.IsActionButton())
+            {
+                if (Context.IsPlayerFree)
+                {
+                    var ao = Game1.player.ActiveObject;
+                    if (ao != null && ao.DisplayName == "Lockpick")
+                    {
+                        var targetOut = ModUtil.GetLocalPlayerFacingTileCoordinate();
+                        var keyOut = Game1.currentLocation.Name + "." + targetOut[0] + "." + targetOut[1];
+                        if (ModUtil.Debug) Monitor.Log("Facing target: " + keyOut);
+
+                        var targetIn = ModUtil.GetLocalPlayerStandingTileCoordinate();
+                        var keyIn = Game1.currentLocation.Name + "." + targetIn[0] + "." + targetIn[1];
+                        if (ModUtil.Debug) Monitor.Log("Standing at: " + keyIn);
+
+                        //check out lock
+                        var cle2 = Config.GetMatchingOutLock(keyOut);
+                        if (cle2 != null)
+                        {
+                            Helper.Input.Suppress(e.Button);
+                            ModUtil.AskQuestion("Use lockpick?", new[]{new Response(keyOut, "Yes"),new Response("No","No")}, QuestionCallbackOutLock);
+                        }
+                        //check in lock
+                        cle2 = Config.GetMatchingInLock(keyIn);
+                        if (cle2 != null)
+                        {
+                            Helper.Input.Suppress(e.Button);
+                            ModUtil.AskQuestion("Use lockpick?", new[] { new Response(keyIn, "Yes"), new Response("No", "No") }, QuestionCallbackInLock);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void QuestionCallbackOutLock(Farmer who, string answerKey)
+        {
+            if (answerKey != "No")
+            {
+                ConfigLockEnd cle2 = Config.GetMatchingOutLock(answerKey);
+                if (new System.Random().Next(100) < 10) //ohno. the pick broke.
+                {
+                    Game1.playSound("axe");
+                    Game1.showRedMessage("The lockpick broke!");
+                    ModUtil.RemoveItemFromLocalPlayer(who.ActiveObject);
+                    return;
+                }
+                Game1.playSound("axchop");
+                who.warpFarmer(new Warp(cle2.MapX, cle2.MapY, cle2.MapName, cle2.MapX, cle2.MapY, false));
+            }
+        }
+
+        public void QuestionCallbackInLock(Farmer who, string answerKey)
+        {
+            if (answerKey != "No")
+            {
+                ConfigLockEnd cle2 = Config.GetMatchingInLock(answerKey);
+                Game1.playSound("axchop");
+                who.warpFarmer(new Warp(cle2.MapX, cle2.MapY, cle2.MapName, cle2.MapX, cle2.MapY, false));
+            }
+        }
+    }
+}
