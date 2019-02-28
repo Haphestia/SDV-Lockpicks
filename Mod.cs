ba0a52e8fb4e3505e7bb02d275d7fe1c6ef531af
bwdymods <bwdymods@gmail.com>
2019-02-27 23:49:26 -0900

    WIP update

--- Mod.cs
+++ Mod.cs
@@ -14,6 +14,11 @@ namespace Lockpicks
         public void EntryDebug() { Debug = true; }
         internal static string Module;
 
+        //all will iterate every character on map and if they can see you, you lose friendship
+        //burgle dresser (clothes, medium gold, medium treasures) (has owners flag, so we know whose 'clothes')
+        //burgle desk (junk, small treasures, big gold)
+        //burgle other (junk, big treasures, small gold)
+
         public override void Entry(IModHelper helper)
         {
             Module = helper.ModRegistry.ModID;
@@ -24,25 +29,18 @@ namespace Lockpicks
             if (Config.ready)
             {
                 Modworks.Items.AddItem(Module, new bwdyworks.BasicItemEntry(this, "lockpick", 30, -300, "Basic", Object.junkCategory, "Lockpick", "Used to bypass locked doors."));
-                Modworks.Items.AddMonsterLoot(Module, new bwdyworks.MonsterLootEntry(this, "Green Slime", "lockpick", 0.1f));                 
+                Modworks.Items.AddMonsterLoot(Module, new bwdyworks.MonsterLootEntry(this, "Green Slime", "lockpick", 0.3f));                 
             }
             helper.Events.Input.ButtonPressed += Input_ButtonPressed;
         }
 
         private void Input_ButtonPressed(object sender, ButtonPressedEventArgs e)
         {
-            if (Debug)
-            {
-                if (e.Button == SButton.NumPad0)
-                {
-                    var id = Modworks.Items.GetModItemId(Module, "lockpick");
-                    if (Debug && id != null) Monitor.Log("lockpick id: " + id.Value);
-
-                    if (id != null) Modworks.Player.GiveItem(id.Value, 1);
-                }
-            }
             if (e.Button.IsActionButton())
             {
+                var targetOut2 = Modworks.Player.GetFacingTileCoordinate();
+                var keyOut2 = Game1.currentLocation.Name + "." + targetOut2[0] + "." + targetOut2[1];
+                Monitor.Log("Facing target: " + keyOut2);
                 if (Context.IsPlayerFree)
                 {
                     var ao = Game1.player.ActiveObject;
@@ -50,11 +48,11 @@ namespace Lockpicks
                     {
                         var targetOut = Modworks.Player.GetFacingTileCoordinate();
                         var keyOut = Game1.currentLocation.Name + "." + targetOut[0] + "." + targetOut[1];
-                        if (Debug) Monitor.Log("Facing target: " + keyOut);
+                        //if (Debug) Monitor.Log("Facing target: " + keyOut);
 
                         var targetIn = Modworks.Player.GetStandingTileCoordinate();
                         var keyIn = Game1.currentLocation.Name + "." + targetIn[0] + "." + targetIn[1];
-                        if (Debug) Monitor.Log("Standing at: " + keyIn);
+                        //if (Debug) Monitor.Log("Standing at: " + keyIn);
 
                         //check out lock
                         var cle2 = Config.GetMatchingOutLock(keyOut);
@@ -80,14 +78,14 @@ namespace Lockpicks
             if (answerKey != "No")
             {
                 ConfigLockEnd cle2 = Config.GetMatchingOutLock(answerKey);
-                if (new System.Random().Next(100) < 10) //ohno. the pick broke.
+                if (Modworks.RNG.NextDouble() > Modworks.Player.GetLuckFactorFloat()) //ohno. the pick broke.
                 {
-                    Game1.playSound("axe");
+                    Game1.playSound("clank");
                     Game1.showRedMessage("The lockpick broke!");
                     Modworks.Player.RemoveItem(who.ActiveObject);
                     return;
                 }
-                Game1.playSound("axchop");
+                Game1.playSound("stoneCrack");
                 who.warpFarmer(new Warp(cle2.MapX, cle2.MapY, cle2.MapName, cle2.MapX, cle2.MapY, false));
             }
         }
@@ -97,7 +95,7 @@ namespace Lockpicks
             if (answerKey != "No")
             {
                 ConfigLockEnd cle2 = Config.GetMatchingInLock(answerKey);
-                Game1.playSound("axchop");
+                Game1.playSound("stoneCrack");
                 who.warpFarmer(new Warp(cle2.MapX, cle2.MapY, cle2.MapName, cle2.MapX, cle2.MapY, false));
             }
         }
